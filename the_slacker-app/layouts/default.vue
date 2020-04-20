0acc998ab630045a693d1f1813650049025e3c87
@higakikeita <higaki.keichan@gmail.com>
2020-04-20 18:54:41 +0900

    仮置きビュー表示

--- the_slacker-app/layouts/default.vue
+++ the_slacker-app/layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app-layout">
-    <div class="sidebar"></div>
+    <div class="sidebar">
+      <p>チャンネル一覧</p>+
+      <p>#general</p>+
+      <p>#random</p>+
+      <p>#random</p>
+    </div>
     <div class="main-content">
       <nuxt />
     </div>
@@ -16,6 +21,11 @@
   width: 300px;
   background: #4a4141;
   height: 100vh;
+  padding: 20px;
+}
+.sidebar p {
+  color: #dddddd;
+  padding-top: 4px;
 }
 
 .main-content {
