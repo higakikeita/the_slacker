a43223ba4f8a5954ff0812b8a61ededfd9761071
@higakikeita <higaki.keichan@gmail.com>
2020-04-20 20:27:04 +0900

    computed

--- the_slacker-app/components/Message.vue
+++ the_slacker-app/components/Message.vue
@@ -3,16 +3,16 @@
     <div class="chats-container">
       <div class="chat-container">
         <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
+          <img v-bind:src="message.user.thumbnail" />
         </div>
         <div class="message-container">
-          <div class="user-name">cohki0305</div>
-          <div class="message">今日も良い天気ですね</div>
+          <div class="user-name">{{ displayName }}</div>
+          <div class="message">{{ message.text }}</div>
         </div>
       </div>
       <div class="chat-container">
         <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
+          <img v-bind:src="message.user.thumbnail" />
         </div>
         <div class="message-container">
           <div class="user-name">cohki0305</div>
@@ -46,3 +46,24 @@
   font-weight: bold;
 }
 </style>
+<script>
+export default {
+  data() {
+    return {
+      message: {
+        text: "今日も良い天気ですね",
+        user: {
+          thumbnail:
+            "https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg",
+          name: "note"
+        }
+      }
+    };
+  },
+  computed: {
+    displayName() {
+      return `@${this.message.user.name}`;
+    }
+  }
+};
+</script>
