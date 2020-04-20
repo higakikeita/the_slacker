1590d0fd2ed977eb4ed07441aaf85e8c114ebbfe
@higakikeita <higaki.keichan@gmail.com>
2020-04-20 18:40:15 +0900

    レイアウト背景の修正

--- the_slacker-app/layouts/default.vue
+++ the_slacker-app/layouts/default.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="app-layout">
+    <div class="sidebar"></div>
+    <div class="main-content">
+      <nuxt />
+    </div>
   </div>
 </template>
 
 <style>
+.app-layout {
+  display: flex;
+}
+
+.sidebar {
+  width: 300px;
+  background: #4a4141;
+  height: 100vh;
+}
+
+.main-content {
+  width: 100%;
+  background: #f1f1f1;
+  height: 100vh;
+}
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,7 +35,9 @@ html {
   box-sizing: border-box;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
