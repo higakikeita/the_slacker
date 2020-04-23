e99f79c7a350749966260b9680347e07055ba88c
@higakikeita <higaki.keichan@gmail.com>
2020-04-23 12:45:01 +0900

    リロードしてもユーザー情報を保持する

--- the_slacker-app/layouts/default.vue
+++ the_slacker-app/layouts/default.vue
@@ -114,6 +114,11 @@ export default {
     }
   },
   mounted() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setUser(user);
+      }
+    });
     db.collection("channels")
       .get()
       .then(querySnapshot => {
